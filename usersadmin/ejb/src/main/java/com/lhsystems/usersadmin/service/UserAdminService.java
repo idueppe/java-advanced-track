d798bb4a8c48b7bb6bc2bb489af580a3940d81d3
idueppe <ingo.dueppe@dueppe.com>
2014-03-18 15:00:56 +0100

    Enhanced UserAdminServiceTest

--- usersadmin/ejb/src/main/java/com/lhsystems/usersadmin/service/UserAdminService.java
+++ usersadmin/ejb/src/main/java/com/lhsystems/usersadmin/service/UserAdminService.java
@@ -3,12 +3,13 @@ package com.lhsystems.usersadmin.service;
 import java.util.List;
 
 import com.lhsystems.usersadmin.domain.User;
+import com.lhsystems.usersadmin.service.spi.UserAlreadyExistsException;
 
 public interface UserAdminService {
 	
 	public List<User> listAllUsers();
 	
-	public void createUser(User user);
+	public void createUser(User user) throws UserAlreadyExistsException;
 	
 	public void updateUser(User user);
 	
--- usersadmin/ejb/src/main/java/com/lhsystems/usersadmin/service/spi/UserAdminServiceBean.java
+++ usersadmin/ejb/src/main/java/com/lhsystems/usersadmin/service/spi/UserAdminServiceBean.java
@@ -1,5 +1,6 @@
 package com.lhsystems.usersadmin.service.spi;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import javax.ejb.Local;
@@ -8,6 +9,7 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import com.lhsystems.usersadmin.doa.UserDao;
+import com.lhsystems.usersadmin.domain.Role;
 import com.lhsystems.usersadmin.domain.User;
 import com.lhsystems.usersadmin.service.UserAdminService;
 
@@ -17,21 +19,41 @@ import com.lhsystems.usersadmin.service.UserAdminService;
 public class UserAdminServiceBean implements UserAdminService {
 
 	@Inject
+	// CDI Annotation
+	// @EJB // EJB Annotation
 	private UserDao userDao;
-	
+
 	@Override
 	public List<User> listAllUsers() {
-		return userDao.findAll();
+
+		List<User> users = userDao.findAll();
+		List<User> result = new LinkedList<>();
+		for (User user : users)
+			if (user.getRole() == Role.USER)
+				result.add(user);
+		return result;
 	}
 
 	@Override
-	public void createUser(User user) {
+	public void createUser(User user) throws UserAlreadyExistsException {
+		User found = userDao.findByUsername(user.getUsername());
+		if (found != null)
+			throw new UserAlreadyExistsException("User " + user.getUsername()
+					+ " already exists");
 		userDao.create(user);
 	}
 
 	@Override
 	public void updateUser(User user) {
 		userDao.update(user);
+	}
+
+	public void resetPasswort(Long id, String newPassword) {
+		User user = userDao.find(id);
+		if (user != null) {
+			user.setPassword(newPassword);
+			userDao.update(user);
+		}
 	}
 
 }
