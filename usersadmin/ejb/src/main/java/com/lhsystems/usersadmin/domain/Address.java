d3c2b26603a4ccce797c043f6d683c6bcdff73e3
idueppe <ingo.dueppe@dueppe.com>
2014-04-29 23:50:28 +0200

    JAT-BER-1 at 2014-04-29

---
+++ usersadmin/ejb/src/main/java/com/lhsystems/usersadmin/domain/Address.java
@@ -0,0 +1,44 @@
+package com.lhsystems.usersadmin.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Address {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	private String street;
+	private String city;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getZip() {
+		return zip;
+	}
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+	private String zip;
+	
+	
+}
---
+++ usersadmin/ejb/src/main/java/com/lhsystems/usersadmin/domain/User.java
@@ -0,0 +1,113 @@
+package com.lhsystems.usersadmin.domain;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tbl_user")
+public class User {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Column(unique=true)
+	private String username;
+	private String fullname;
+	private String password;
+    
+	private boolean active;
+	
+	private Date lastPasswordChanged;
+	private Date lastLogin;
+	
+	@Enumerated(EnumType.STRING)
+	private Role role;
+	
+	@OneToOne
+	private Address address;
+	
+
+	public String getUsername() {
+		return username;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getFullname() {
+		return fullname;
+	}
+
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public Date getLastPasswordChanged() {
+		return lastPasswordChanged;
+	}
+
+	public void setLastPasswordChanged(Date lastPasswordChanged) {
+		this.lastPasswordChanged = lastPasswordChanged;
+	}
+
+	public Date getLastLogin() {
+		return lastLogin;
+	}
+
+	public void setLastLogin(Date lastLogin) {
+		this.lastLogin = lastLogin;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	
+	
+}
