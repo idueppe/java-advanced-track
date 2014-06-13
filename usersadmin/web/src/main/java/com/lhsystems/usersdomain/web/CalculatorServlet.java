c3bdb615e0db6e67402bc9e4fb696fbfb680bffd
idueppe <ingo.dueppe@dueppe.com>
2014-06-13 18:16:09 +0200

    Fixed Stateful Session Bean Handling

--- usersadmin/web/src/main/java/com/lhsystems/usersdomain/web/CalculatorServlet.java
+++ usersadmin/web/src/main/java/com/lhsystems/usersdomain/web/CalculatorServlet.java
@@ -2,47 +2,64 @@ package com.lhsystems.usersdomain.web;
 
 import java.io.IOException;
 
-import javax.ejb.EJB;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.lhsystem.usersadmin.security.service.Calculator;
 
 @WebServlet("/calc")
 public class CalculatorServlet extends HttpServlet {
 
+	private static final String CALCULATOR_SESSION_KEY = "my_calculator_session_key";
 	private static final long serialVersionUID = 1L;
 
-	@EJB
-	private Calculator calculator;
-	
+	// @EJB -- doesn't work with stateful session beans
+	// private Calculator calculator;
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		
+
 		String operation = req.getParameter("operation");
 		String value = req.getParameter("value");
-		
-		long sum = calculate(operation, value);
-		resp.getOutputStream().println("Sum is "+sum);
+
+		long sum = calculate(operation, value, getCalculator(req));
+		resp.getOutputStream().println("Sum is " + sum);
 	}
 
-	private long calculate(String operation, String value) {
-		switch (operation)
+	private Calculator getCalculator(HttpServletRequest req) {
+		HttpSession session = req.getSession(true);
+		Calculator calculator = (Calculator) session.getAttribute(CALCULATOR_SESSION_KEY);
+		if (calculator == null)
 		{
-		case "add" : 
+			try {
+				InitialContext context = new InitialContext();
+				calculator = (Calculator) context.lookup("java:app/usersadmin-ejb/CalculatorBean!com.lhsystem.usersadmin.security.service.Calculator");
+				session.setAttribute(CALCULATOR_SESSION_KEY, calculator);
+			} catch (NamingException e) {
+				e.printStackTrace();
+			}
+		}
+		return calculator;
+	}
+
+	private long calculate(String operation, String value, Calculator calculator) {
+		switch (operation) {
+		case "add":
 			calculator.add(Long.valueOf(value));
 			break;
-		case "sub" :
+		case "sub":
 			calculator.sub(Long.valueOf(value));
 			break;
 		}
-		
+
 		return calculator.sum();
 	}
 
-	
 }
