f27b6639efaff605eef25ad0732d8d7a897230a7
idueppe <ingo.dueppe@dueppe.com>
2014-06-13 18:13:56 +0200

    Add callback functions

--- usersadmin/ejb/src/main/java/com/lhsystem/usersadmin/security/service/CalculatorBean.java
+++ usersadmin/ejb/src/main/java/com/lhsystem/usersadmin/security/service/CalculatorBean.java
@@ -1,14 +1,47 @@
 package com.lhsystem.usersadmin.security.service;
 
+import java.util.concurrent.TimeUnit;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.ejb.Local;
+import javax.ejb.PostActivate;
+import javax.ejb.PrePassivate;
 import javax.ejb.Remove;
 import javax.ejb.Stateful;
+import javax.ejb.StatefulTimeout;
 
 @Stateful
 @Local(Calculator.class)
+@StatefulTimeout(unit=TimeUnit.MINUTES,value=10)
 public class CalculatorBean implements Calculator {
 
 	private long value;
+	
+	@PreDestroy
+	public void preDestroy()
+	{
+		System.out.println("calculator predestroy.");
+	}
+	
+	@PrePassivate
+	public void prePassivate()
+	{
+		System.out.println("calculator passivate");
+	}
+	
+	@PostActivate
+	public void postActivate()
+	{
+		System.out.println("calculator activate");
+	}
+	
+	@PostConstruct
+	public void postConstructor()
+	{
+		System.out.println("calculator constructed");
+		
+	}
 	
 	@Override
 	public void add(long value) {
