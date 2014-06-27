1c0d2af11db792e11872f9171436dee378cf0c59
idueppe <ingo.dueppe@dueppe.com>
2014-06-27 14:36:56 +0200

    Add html template for user list and user add form.

--- usersadmin/web/src/main/java/com/lhsystems/usersadmin/web/UsersServlet.java
+++ usersadmin/web/src/main/java/com/lhsystems/usersadmin/web/UsersServlet.java
@@ -2,6 +2,7 @@ package com.lhsystems.usersadmin.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
@@ -27,17 +28,46 @@ public class UsersServlet extends HttpServlet {
 
 		PrintWriter out = resp.getWriter();
 
-		out.println("<html><body><h1>Users</h1>");
-		out.println("<ul>");
+		// TODO call print... methods 
+	
+	}
+	
+	
 
-		for (UserDto user : usersService.listUsers()) {
+	private void printHtmlHeader(PrintWriter out) {
+		out.println("<html><body>");
+	}
+
+	private void printUsersListe(PrintWriter out, List<UserDto> users) {
+		out.println("<h1>Users List</h1>");
+		out.println("		<h4>Liste:</h4> ");
+		out.println("<ul>");
+		for (UserDto user : users) {
 			out.println("<li>");
 			out.println(user.toString());
 			out.println("</li>");
 		}
-
 		out.println("</ul>");
-		out.println("</body></html>");
+	}
+
+	private void printAddUserForm(PrintWriter out) {
+		out.println("		<h4>Add User:</h4> ");
+		out.println("		<form method='post'> ");
+		out.println("			<label>Username</label><br/><input type='text' name='username'/> <br/> ");
+		out.println("			<label>Email</label><br/><input type='text' name='email' /> <br/>");
+		out.println("			<label>Role</label>");
+		out.println("					<select name='role'>");
+		out.println("						<option value='user'>User</option>");
+		out.println("						<option value='admin'>Admin</option>");
+		out.println("						<option value='attendee'>Attendee</option>");
+		out.println("					</select>");
+		out.println("			<input type='submit' value='add'/> ");
+		out.println("		</form> ");
+	}
+
+	private void printHtmlFooter(PrintWriter out) {
+		out.println("	</body> ");
+		out.println("</html> ");
 	}
 
 }
