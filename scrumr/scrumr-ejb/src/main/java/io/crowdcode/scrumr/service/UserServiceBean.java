2cfb5adc5c3d9e31189bb2818bdfbc5921f0edb4
idueppe <ingo.dueppe@dueppe.com>
2014-07-16 15:06:36 +0200

    Enhanced UserServiceBean and tests

--- scrumr/scrumr-ejb/src/main/java/io/crowdcode/scrumr/service/UserServiceBean.java
+++ scrumr/scrumr-ejb/src/main/java/io/crowdcode/scrumr/service/UserServiceBean.java
@@ -1,14 +1,34 @@
 package io.crowdcode.scrumr.service;
 
+import io.crowdcode.scrumr.dao.UserDao;
+import io.crowdcode.scrumr.exception.InvalidUsernameException;
+import io.crowdcode.scrumr.model.User;
+
 import java.util.UUID;
 
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.apache.commons.lang3.StringUtils;
+
+@Named
+@Stateless
 public class UserServiceBean
 {
+	@Inject
+	private UserDao userDao;
 
-	public String registerUser(String username, String email)
+	public String registerUser(String username, String email) throws InvalidUsernameException
 	{
 		// TODO Auto-generated method stub
-		return UUID.randomUUID().toString();
+		if (StringUtils.isBlank(username))
+			throw new InvalidUsernameException(username);
+
+		User user = new User();
+		userDao.persist(user);
+		
+		return user.getId();//UUID.randomUUID().toString();
 	}
 
 }
--- scrumr/scrumr-ejb/src/test/java/io/crowdcode/scrumr/service/UserServiceTest.java
+++ scrumr/scrumr-ejb/src/test/java/io/crowdcode/scrumr/service/UserServiceTest.java
@@ -1,12 +1,20 @@
 package io.crowdcode.scrumr.service;
 
+import static io.crowdcode.scrumr.test.TestUtils.with;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import io.crowdcode.scrumr.dao.UserDao;
+import io.crowdcode.scrumr.exception.InvalidUsernameException;
+import io.crowdcode.scrumr.model.User;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -14,17 +22,29 @@ public class UserServiceTest
 {
 	@InjectMocks
 	private UserServiceBean userService;
+	
+	@Mock
+	private UserDao userDao; // = Mockito.mock(UserDao.class);
 
 	@Test
 	public void testRegisterUser() throws Exception
 	{
-		// Arrage
+		String expectedId = "xyz123";
+		// Arrange
+		doAnswer(with(expectedId)).when(userDao).persist(any(User.class));
 		String username = "junit_test_user";
 		String email = "junit@junit.org";
 		// Act
 		String id = userService.registerUser(username, email);
 		// Assert
-		assertThat(id, is(notNullValue()));
+		assertThat(id, is(expectedId));
+		verify(userDao, times(1)).persist(any(User.class));
 	}
-	
+
+	@Test(expected = InvalidUsernameException.class)
+	public void testInvalidUsernameException() throws Exception
+	{
+		userService.registerUser("", "dsaf@dfsdf.com");
+	}
+
 }
