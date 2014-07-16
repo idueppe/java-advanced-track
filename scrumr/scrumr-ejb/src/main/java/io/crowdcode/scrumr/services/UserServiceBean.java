a73e1718f9b575ea757ecdf30f806d160f524891
idueppe <ingo.dueppe@dueppe.com>
2014-07-16 12:37:16 +0200

    Add initial test class

---
+++ scrumr/scrumr-ejb/src/main/java/io/crowdcode/scrumr/services/UserServiceBean.java
@@ -0,0 +1,14 @@
+package io.crowdcode.scrumr.services;
+
+import java.util.UUID;
+
+public class UserServiceBean
+{
+
+	public String registerUser(String username, String email)
+	{
+		// TODO Auto-generated method stub
+		return UUID.randomUUID().toString();
+	}
+
+}
---
+++ scrumr/scrumr-ejb/src/test/java/io/crowdcode/scrumr/services/UserServiceTest.java
@@ -0,0 +1,30 @@
+package io.crowdcode.scrumr.services;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserServiceTest
+{
+	@InjectMocks
+	private UserServiceBean userService;
+
+	@Test
+	public void testRegisterUser() throws Exception
+	{
+		// Arrage
+		String username = "junit_test_user";
+		String email = "junit@junit.org";
+		// Act
+		String id = userService.registerUser(username, email);
+		// Assert
+		assertThat(id, is(notNullValue()));
+	}
+	
+}
