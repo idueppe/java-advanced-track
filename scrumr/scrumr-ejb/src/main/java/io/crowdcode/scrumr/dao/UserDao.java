bfe72bc629283046a47ef85959f5c1ecb9cad423
idueppe <ingo.dueppe@dueppe.com>
2014-07-18 15:33:39 +0200

    Add UserDao interface

---
+++ scrumr/scrumr-ejb/src/main/java/io/crowdcode/scrumr/dao/UserDao.java
@@ -0,0 +1,23 @@
+package io.crowdcode.scrumr.dao;
+
+import io.crowdcode.scrumr.model.User;
+
+import java.util.List;
+
+public interface UserDao
+{
+
+	public void persist(User user);
+
+	/**
+	 * @return user instance of null if user not exists
+	 */
+	public User findUserByEmail(String email);
+
+	public List<User> findAdmins();
+
+	public void remove(User admin);
+
+	public List<User> findAllUsers();
+
+}
