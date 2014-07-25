0e22dd85fa14acd90914f7924b4c2f685460e2c9
idueppe <ingo.dueppe@dueppe.com>
2014-07-25 19:13:33 +0200

    Changed ProjectDto to just hold the emails for the members.

--- scrumr/scrumr-api/src/main/java/io/crowdcode/scrumr/dto/ProjectDto.java
+++ scrumr/scrumr-api/src/main/java/io/crowdcode/scrumr/dto/ProjectDto.java
@@ -5,40 +5,37 @@ import java.util.List;
 
 public class ProjectDto
 {
-	
+
 	private String id;
 
 	private String name;
-	
-	private String description;
-	
-	private UserDto productOwner;
-	
-	private UserDto scrumMaster;
 
-	private List<UserDto> developers = new ArrayList<>();
-	
+	private String description;
+
+	private String productOwnerMail;
+
+	private String scrumMasterMail;
+
+	private List<String> developerMails = new ArrayList<>();
+
 	public String getId()
 	{
 		return id;
 	}
-	
+
 	public void setId(String id)
 	{
 		this.id = id;
 	}
 
-	public String getName() {
+	public String getName()
+	{
 		return name;
 	}
 
-	public void setName(String name) {
+	public void setName(String name)
+	{
 		this.name = name;
-	}
-
-	public ProjectDto withName(String name) {
-		setName(name);
-		return this;
 	}
 
 	public String getDescription()
@@ -50,67 +47,35 @@ public class ProjectDto
 	{
 		this.description = description;
 	}
-	
-	public ProjectDto withDescription(String description)
+
+	public String getProductOwnerMail()
 	{
-		setDescription(description);
-		return this;
+		return productOwnerMail;
 	}
 
-	public UserDto getProductOwner()
+	public void setProductOwnerMail(String productOwner)
 	{
-		return productOwner;
+		this.productOwnerMail = productOwner;
 	}
 
-	public void setProductOwner(UserDto productOwner)
+	public String getScrumMasterMail()
 	{
-		this.productOwner = productOwner;
-	}
-	
-	public ProjectDto withProductOwner(UserDto productOwner)
-	{
-		setProductOwner(productOwner);
-		return this;
+		return scrumMasterMail;
 	}
 
-	public UserDto getScrumMaster()
+	public void setScrumMasterMail(String scrumMaster)
 	{
-		return scrumMaster;
+		this.scrumMasterMail = scrumMaster;
 	}
 
-	public void setScrumMaster(UserDto scrumMaster)
+	public List<String> getDeveloperMails()
 	{
-		this.scrumMaster = scrumMaster;
-	}
-	
-	public ProjectDto withScrumMaster(UserDto scrumMaster)
-	{
-		setScrumMaster(scrumMaster);
-		return this;
+		return developerMails;
 	}
 
-	public List<UserDto> getDevelopers()
+	public void setDeveloperMails(List<String> developerMails)
 	{
-		return developers;
+		this.developerMails = developerMails;
 	}
 
-	public void setDevelopers(List<UserDto> developers)
-	{
-		this.developers = developers;
-	}
-	
-	public ProjectDto addDeveloper(UserDto developer) 
-	{
-		getDevelopers().add(developer);
-		return this;
-	}
-
-	@Override
-	public String toString()
-	{
-		return "Project [name=" + name + ", description=" + description + ", productOwner=" + productOwner + ", scrumMaster=" + scrumMaster
-				+ ", developers=" + developers + "]";
-	}
-
-	
 }
---
+++ scrumr/scrumr-ejb/src/main/java/io/crowdcode/scrumr/converters/ProjectDtoConverter.java
@@ -0,0 +1,44 @@
+package io.crowdcode.scrumr.converters;
+
+import io.crowdcode.scrumr.dto.ProjectDto;
+import io.crowdcode.scrumr.model.Project;
+import io.crowdcode.scrumr.model.User;
+
+import javax.inject.Named;
+
+@Named
+public class ProjectDtoConverter extends AbstractDefaultConverter<Project, ProjectDto>
+{
+
+	@Override
+	protected ProjectDto newTargetInstance()
+	{
+		return new ProjectDto();
+	}
+
+	@Override
+	protected void copyProperties(Project source, ProjectDto target)
+	{
+		target.setName(source.getName());
+		target.setDescription(source.getDescription());
+		
+		if (source.getProductOwner() != null)
+		{
+			target.setProductOwnerMail(source.getProductOwner().getEmail());
+		}
+		
+		if (source.getScrumMaster() != null)
+		{
+			target.setScrumMasterMail(source.getScrumMaster().getEmail());
+		}
+
+		if (source.getDevelopers() != null)
+		{
+			for (User user : source.getDevelopers())
+			{
+				target.getDeveloperMails().add(user.getEmail());
+			}
+		}
+	}
+
+}
