92cc99a870b570620914871c1d8e69c6714d189b
idueppe <ingo.dueppe@dueppe.com>
2014-08-03 17:21:17 +0200

    Initial scrumr-wicket setup

---
+++ scrumr/scrumr-wicket/src/main/java/io/crowdcode/scrumr/HomePage.java
@@ -0,0 +1,20 @@
+package io.crowdcode.scrumr;
+
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.WebPage;
+
+public class HomePage extends WebPage {
+	private static final long serialVersionUID = 1L;
+
+	public HomePage(final PageParameters parameters) {
+		super(parameters);
+
+		add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
+		
+		add(new Label("pageTitle", "Wicket-Scrumr"));
+
+		// TODO Add your page's components here
+
+    }
+}
