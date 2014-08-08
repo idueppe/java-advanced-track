260ed3a109dbde8a29065c488c445dbb4c76710a
idueppe <ingo.dueppe@dueppe.com>
2014-08-08 10:59:33 +0200

    Fixed after merge!

--- scrumr/scrumr-wicket/src/main/java/io/crowdcode/scrumr/ui/HomePage.java
+++ scrumr/scrumr-wicket/src/main/java/io/crowdcode/scrumr/ui/HomePage.java
@@ -1,22 +1,15 @@
 package io.crowdcode.scrumr.ui;
 
-import io.crowdcode.scrumr.ui.template.TemplatePage;
-
-import java.util.logging.Logger;
-
+import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
-public class HomePage extends TemplatePage {
-	private static final Logger LOG = Logger.getLogger(HomePage.class.getName());
-	
+public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
 
 	public HomePage(final PageParameters parameters) {
 		super(parameters);
 		
-		getNavigationPanel().setActiveLink(getNavigationPanel().getHomeLink());
-
 		add(new Label("pageTitle", "Wicket-Scrumr"));
 		
     }
